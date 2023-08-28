1417a514be02528b390d3f84db7f407508745d7f
yamadaOliva <vietanh87.forwork@gmail.com>
2023-08-29 01:14:51 +0700

    set up and create header

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+const bodyParser = require('body-parser')
+server.use(middlewares)
+server.use(router)
+server.use(bodyParser.json())
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+})
+
